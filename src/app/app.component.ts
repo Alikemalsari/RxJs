598126bf4d08a3bbc71bc09fe4155d1d0fc9f690
Ali Kemal SARI <a.kemalsari46@gmail.com>
2024-05-09 15:41:14 +0300

    Created RxJs Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RxJs';
+
+  ngOnInit(){
+    const observable=new Observable/*<number>*/(a=>{
+    // eger <number> seklinde tip belitrisek sadece o tipe ait verileri olusturabiliriz
+    // new Observable ile abone olunabilir nesne olusturulur
+   a.next(1);
+   a.next(2);
+   a.next(3);
+   a.next("Öyle bir kayarım ki hayatından, yapacağın tek şey dilek tutmak olur benim ardımdan... (Kıvanc Tatlıtuğ) ");
+   //data.next ile akışa veri aktarilacak veriler belirlenir
+   a.complete();
+       // compelete fonksiyonuna kadar akisa eklenecek datalarimi tanimlayabilirim
+     // akışa başka data vermeyeceksek burada sonlandırırz. BNöylece son datanında gönderildigini tüketixcimiz bilir
+  });
+
+
+ observable.subscribe(data=>console.log(data));
+// subscribe ile observer (burada data degiskeni ile tanimladik) yani tüketici tanımlayarak nesnelerimiz akisa verilir
+
+/*
+const observer =function(data:any){
+  console.log(data)
+}
+
+observable.subscribe(observer)
+*/
+
+console.log("************************  SUBJECT KONUSUNA GEÇİYORUZ   **************************")
+
+const subject=new Subject();
+subject.subscribe(data=>console.log(`ObserverA ${data}`))
+subject.subscribe(data=>console.log(`ObserverB ${data}`))
+subject.next(3);
+subject.next(354645);
+subject.subscribe(data=>console.log(`ObserverC ${data}`))
+subject.next("subject datası gönderiyorum");
+subject.next(56);
+
+
+  }
+
 }
